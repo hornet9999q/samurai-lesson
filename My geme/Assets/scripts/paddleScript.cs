245c0f0bb986715a6739418b1ea0c44c1cb72897
yuu <hornet9999q@yahoo.co.jp>
2018-10-20 17:46:15 +0900

    My game追加

---
+++ My geme/Assets/scripts/paddleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Debug.Log(transform.position.x);
+        
+	}
+}
