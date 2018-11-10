f84c9a4c5a223b03a20e917f72ba8d655c926226
yuu <hornet9999q@yahoo.co.jp>
2018-11-10 14:28:10 +0900

    first commit

---
+++ My game2/New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Person p = new Person();
+        p.firstname = "太郎";
+        p.lastname = "山田";
+        Debug.Log(p.GetFullName("★"));
+
+	}
+	
+    class Person{
+        public string firstname;
+        public string lastname;
+
+        public string GetFullName(string separator){
+            return this.lastname + separator + this.firstname;
+        }
+        }
+    
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ My game2/New Unity Project/Assets/lesson2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lesson2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int a = 7;
+        if (a < 5){
+            Debug.Log("aは5よりちいさい");
+        }
+        else{
+            Debug.Log("aは5より大きい");
+        }
+        Debug.Log("終了");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
